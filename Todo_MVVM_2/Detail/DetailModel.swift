63452204cf6786c5c39c64a1e66ef075cceb4214
Asady1113 <asadyswift@gmail.com>
2023-11-29 16:54:05 +0900

    Detailに関するファイルを作成

---
+++ Todo_MVVM_2/Detail/DetailModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailModel.swift
+//  Todo_MVVM_2
+//
+//  Created by 浅田智哉 on 2023/11/29.
+//
+
+import Foundation
+
+class DetailModel {
+    
+}
--- Todo_MVVM_2/Detail/DetailViewController.swift
+++ Todo_MVVM_2/Detail/DetailViewController.swift
@@ -6,19 +6,47 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet private weak var updateButton: UIButton!
+    @IBOutlet private weak var deleteButton: UIButton!
+    @IBOutlet private weak var titleTextField: UITextField!
+    @IBOutlet private weak var memoTextView: UITextView!
+    
     private var selectedTask = Task()
+    
+    private let detailViewModel = DetailViewModel()
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(selectedTask)
+        
+        displaySelectedTask()
+        // ここでバインディングする
+        bindInput()
+        bindOutput()
     }
     
     func setSelectedTask(selectedTask: Task) {
         self.selectedTask = selectedTask
+    }
+    
+    // 入力に関するバインディング(UIからの入力をViewModelに伝達)
+    private func bindInput() {
+        
+    }
+    
+    // 出力に関するバインディング(ViewModelから来た値をUIに表示)
+    private func bindOutput() {
+        
+    }
+    
+    private func displaySelectedTask() {
+        titleTextField.text = selectedTask.title
+        titleTextField.text = selectedTask.memo
     }
 
 }
